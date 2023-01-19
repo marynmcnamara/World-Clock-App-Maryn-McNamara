7160b74ed3f6c629ce7295e2a652ec45d6ad4adf
marynmcnamara <119798224+marynmcnamara@users.noreply.github.com>
2023-01-19 17:18:57 -0500

    added current time zone functionality

--- src/app.js
+++ src/app.js
@@ -25,6 +25,9 @@ setInterval(updateParisTime, 1000);
 //Update City
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
